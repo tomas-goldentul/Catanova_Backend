ff3b861406406028c1348f539cac82fa8c9d035a
Max <95097703@est.ort.edu.ar>
2026-05-11 10:24:08 -0300

    Nuestras cabezas estan pensando

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const port = 3000;          
 
 app.use(express.json());
 
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+})
